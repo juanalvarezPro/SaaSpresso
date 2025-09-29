c8cc8e5b65236cfa430b5af15d2addc92f5916fa
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-09-29 14:40:49 -0500

    Translate documentation titles and descriptions to Spanish, update navigation items, and remove obsolete files related to landing and configuration sections.

--- config/docs.ts
+++ config/docs.ts
@@ -3,68 +3,24 @@ import { DocsConfig } from "types";
 export const docsConfig: DocsConfig = {
   mainNav: [
     {
-      title: "Documentation",
+      title: "Documentacion",
       href: "/docs",
     },
     {
-      title: "Guides",
+      title: "Guias",
       href: "/guides",
     },
   ],
   sidebarNav: [
     {
-      title: "Getting Started",
+      title: "Comenzando",
       items: [
         {
-          title: "Introduction",
+          title: "Introduccion",
           href: "/docs",
         },
-        {
-          title: "Installation",
-          href: "/docs/installation",
-        },
       ],
     },
-    {
-      title: "Configuration",
-      items: [
-        {
-          title: "Authentification",
-          href: "/docs/configuration/authentification",
-        },
-        {
-          title: "Blog",
-          href: "/docs/configuration/blog",
-        },
-        {
-          title: "Components",
-          href: "/docs/configuration/components",
-        },
-        {
-          title: "Config files",
-          href: "/docs/configuration/config-files",
-        },
-        {
-          title: "Database",
-          href: "/docs/configuration/database",
-        },
-        {
-          title: "Email",
-          href: "/docs/configuration/email",
-        },
-        {
-          title: "Layouts",
-          href: "/docs/configuration/layouts",
-        },
-        {
-          title: "Markdown files",
-          href: "/docs/configuration/markdown-files",
-        },
-        {
-          title: "Subscriptions",
-          href: "/docs/configuration/subscriptions",
-        },
-      ],
-    },
+
   ],
 };
