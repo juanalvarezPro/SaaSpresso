c9ceaceb83f64d2cfa58038a9b82ec5fb4b0b896
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-09-29 13:00:06 -0500

    Repo inspirado en @next-saas-stripe-starter

---
+++ config/blog.ts
@@ -0,0 +1,29 @@
+export const BLOG_CATEGORIES: {
+  title: string;
+  slug: "news" | "education";
+  description: string;
+}[] = [
+  {
+    title: "News",
+    slug: "news",
+    description: "Updates and announcements from Next SaaS Starter.",
+  },
+  {
+    title: "Education",
+    slug: "education",
+    description: "Educational content about SaaS management.",
+  },
+];
+
+export const BLOG_AUTHORS = {
+  juanalvarezpro: {
+    name: "juanalvarezpro",
+    image: "/_static/avatars/juanalvarezpro.png",
+    twitter: "miickasmt",
+  },
+  shadcn: {
+    name: "shadcn",
+    image: "/_static/avatars/shadcn.jpeg",
+    twitter: "shadcn",
+  },
+};
