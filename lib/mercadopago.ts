34f5539912889ad17088334950347843149ee8da
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-09-30 10:27:02 -0500

    Add MerchantOrder and Payment exports to MercadoPago module: enhance functionality by integrating new classes for order and payment management.

--- lib/mercadopago.ts
+++ lib/mercadopago.ts
@@ -1,4 +1,4 @@
-import { MercadoPagoConfig, Preference, Customer } from "mercadopago";
+import { MercadoPagoConfig, Preference, Customer, MerchantOrder, Payment } from "mercadopago";
 import { env } from "@/env.mjs";
 
 // Initialize Mercado Pago configuration
@@ -6,12 +6,16 @@ const client = new MercadoPagoConfig({
   accessToken: env.MERCADOPAGO_ACCESS_TOKEN,
   options: {
     timeout: 5000,
-    idempotencyKey: "abc",
+
   },
 });
+
+
 
 // Initialize services
 export const preference = new Preference(client);
 export const customer = new Customer(client);
+export const merchantOrder = new MerchantOrder(client);
+export const payment = new Payment(client);
 
 export { client as mercadopago };
